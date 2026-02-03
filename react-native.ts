77a99b4f01847781d64daa469d88567ea00b96eb
Rahul Saini <om.ks.rs@gmail.com>
2026-02-03 22:50:03 +0530

    add npm publish related stuff

--- react-native.ts
+++ react-native.ts
@@ -1,1 +1,13 @@
-export { useGraphqlClientDevtool } from './src/react-native/useGraphqlClientDevtool';
+export let useGraphqlClientDevtool: typeof import('./src/react-native/useGraphqlClientDevtool').useGraphqlClientDevtool;
+
+const isWeb =
+    typeof window !== 'undefined' && window.navigator.product !== 'ReactNative';
+const isDev = process.env.NODE_ENV !== 'production';
+const isServer = typeof window === 'undefined';
+
+if (isDev && !isWeb && !isServer) {
+    useGraphqlClientDevtool =
+        require('./src/react-native/useGraphqlClientDevtool').useGraphqlClientDevtool;
+} else {
+    useGraphqlClientDevtool = () => null as any;
+}
